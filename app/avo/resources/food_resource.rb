d7a9a123ca9a2f681b38131660c52c1ad647fc52
Buyaki <rittahbuyaki@gmail.com>
2022-12-09 09:02:16 +0300

    Add Food resource and add relationship between the food and user

---
+++ app/avo/resources/food_resource.rb
@@ -0,0 +1,15 @@
+class FoodResource < Avo::BaseResource
+  self.title = :id
+  self.includes = []
+  # self.search_query = -> do
+  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  field :id, as: :id
+  # Fields generated from the model
+  field :name, as: :text
+  field :measurement_unit, as: :text
+  field :price, as: :number
+  field :quantity, as: :number
+  # add fields here
+end
